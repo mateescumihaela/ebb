cc311dfa2cad91e3ad55ca3b58678c9fdcda76ec
Mihaela Mateescu <miha.mateescu@gmail.com>
2020-05-16 16:18:05 +0100

    updated bcrypt

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 const User = require('../models/user.js');
 const Company = require('../models/company');
